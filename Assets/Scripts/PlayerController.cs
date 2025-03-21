b31e362f513c73d81c4dfa674a6e67fa596904f5
Maxence <150053499+Scrapsss@users.noreply.github.com>
2025-03-21 12:21:10 +0100

    -SYSTEM- [ADD] Shadow Zone et Bug Fixing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class PlayerMovement : MonoBehaviour
@@ -33,9 +34,13 @@ public class PlayerMovement : MonoBehaviour
     //WallJump System
     private bool isWallLeft;
     private bool isWallRight;
-    private bool WallJumpLock;
+    private bool MovementLock;
 
     private bool isRoof;
+    private bool ShadowTP_Stance;
+
+    //ShadowSystem
+    private bool inShadow;
 
     void Start()
     {
@@ -51,6 +56,7 @@ public class PlayerMovement : MonoBehaviour
     {
         Move();
         Jump();
+        ShadowTeleport();
     }
 
     //Gestion des déplacements et de l'orientation du personnage
@@ -60,11 +66,14 @@ public class PlayerMovement : MonoBehaviour
         var hit = Physics2D.BoxCast(transform.position,Vector2.one, 0, direction, _distanceDW, detectWall);
 
 
-        if (hit.collider != null)
+        if ( (hit.collider != null) && !(isWallLeft || isWallRight) )
+        {
             return;
+        }
+            
         
         //Quand on sort d'un saut mural on ne peut pas bouger pendant un certain moment
-        if (!WallJumpLock)
+        if (!MovementLock)
         { 
             //Déplacement horizontaux
             if (Input.GetButton("Horizontal") && isGrounded && !isSprinting)
@@ -89,7 +98,7 @@ public class PlayerMovement : MonoBehaviour
                 rb.linearVelocityY = Input.GetAxisRaw("Vertical") * _speed;
             }
 
-        if (Input.GetButtonUp("Vertical") && ( isWallLeft || isWallRight) && !isGrounded )
+        if (Input.GetButtonUp("Vertical") && (isWallLeft || isWallRight) && !isGrounded)
         {
             rb.linearVelocityY = 0;
         }
@@ -138,44 +147,91 @@ public class PlayerMovement : MonoBehaviour
     //Fonction pour sauter
     void Jump()
     {
-        //Saut mural
-        //Wall jump vers la droite
-        if (Input.GetButtonDown("Jump") && isWallLeft && !isGrounded)
+        if (!MovementLock)
         {
-            rb.linearVelocityY = _forceJump;
+            //Saut mural
+            //Wall jump vers la droite
+            if (Input.GetButtonDown("Jump") && isWallLeft && !isGrounded)
+            {
+                rb.linearVelocityY = _forceJump;
 
-            //On force le personnage à s'éloigner du mur
-            rb.linearVelocityX = 1 * _speed;
+                //On force le personnage à s'éloigner du mur
+                rb.linearVelocityX = 1 * (_speed * 2);
 
-            //Ici on viens bloquer les contrôle de déplacement pendant quelques temps pour laisser le personnage se détacher du mur
-            WallJumpLock = true;
-            StartCoroutine(JumpMovementCooldown());
+                //Ici on viens bloquer les contrôle de déplacement pendant quelques temps pour laisser le personnage se détacher du mur
+                MovementLock = true;
+                StartCoroutine(JumpMovementCooldown());
+            }
+            //WallJump vers la gauche
+            else if (Input.GetButtonDown("Jump") && isWallRight && !isGrounded)
+            {
+                rb.linearVelocityY = _forceJump;
+                rb.linearVelocityX = -1 * (_speed * 2);
+
+                MovementLock = true;
+                StartCoroutine(JumpMovementCooldown());
+            }
+
+            //Impossible de sauter si on est contre un plafond (pour éviter de consommer les charges de sauts )
+            //Saut normal 
+            else if (Input.GetButtonDown("Jump") && _currentJump < _limiteJump && !isRoof)
+            {
+                rb.linearVelocityY = _forceJump;
+                _currentJump++;
+            }
         }
-        //WallJump vers la gauche
-        else if (Input.GetButtonDown("Jump") && isWallRight && !isGrounded)
-        {
-            rb.linearVelocityY = _forceJump;
-            rb.linearVelocityX = -1 * _speed;
-
-            WallJumpLock = true;
-            StartCoroutine(JumpMovementCooldown());
-        }
-
-        //Impossible de sauter si on est contre un plafond (pour éviter de consommer les charges de sauts )
-        //Saut normal 
-        else if (Input.GetButtonDown("Jump") && _currentJump < _limiteJump && !isRoof)
-        {
-            rb.linearVelocityY = _forceJump;
-            _currentJump++;
-        }
+        
     }
 
     //On patiente 0.25 secondes avant de débloquer les contrôle de déplacements
     IEnumerator JumpMovementCooldown()
     {
         yield return new WaitForSeconds(0.25f);
-        WallJumpLock = false;
+        MovementLock = false;
     }
+
+    //------- Shadow Teleport Power -------
+
+    private void ShadowTeleport()
+    {
+        if (inShadow)
+        {
+            //C'est la touche E
+            if (Input.GetButtonDown("Fire2") && !ShadowTP_Stance && isGrounded)
+            {
+                ShadowTP_Stance = true;
+            }
+            else if (Input.GetButtonDown("Fire2") && ShadowTP_Stance)
+            {
+                ShadowTP_Stance = false;
+                MovementLock = false;
+            }
+
+            //Gestion de la TP
+            if (ShadowTP_Stance)
+            {
+                MovementLock = true;
+
+                //Ici on vient chercher la position de notre souris par rapport au monde
+                Vector3 screenPoint = Input.mousePosition;
+                
+                //On précise la distance entre la caméra et le monde qui est de 10 unités
+                screenPoint.z = 10;
+                Vector3 worldPoint = Camera.main.ScreenToWorldPoint(screenPoint);
+
+                //Et ensuite on dessine une ligne entre le joueur et la position de la souris
+                Debug.DrawLine(transform.position, worldPoint);
+
+
+            }
+        }
+        
+    }
+
+
+
+
+    //------- Gestion des Collisions ---------
 
     //Dès qu'on quitte les collision nos marqueurs passent en false
     private void OnCollisionExit2D(Collision2D collision)
@@ -225,17 +281,36 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 normal = collision.GetContact(0).normal;
 
-        //Si c'est le haut de la collision alors on peut marcher dessus
-            if (normal == Vector3.up)
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
         {
-            isGrounded = true;
-            _currentJump = 0;
+            //Si c'est le haut de la collision alors on peut marcher dessus
+            if (normal == Vector3.up)
+            {
+                isGrounded = true;
+                _currentJump = 0;
+            }
         }
+            
     }
 
     //Fonction de récolte de collectibles
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        collision.gameObject.SetActive(false);
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Collectible"))
+            collision.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //Ici on check si on est dans une ombre
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Shadow"))
+        {
+            inShadow = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        inShadow = false;
     }
 }
