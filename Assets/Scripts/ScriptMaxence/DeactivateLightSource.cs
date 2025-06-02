da1c851914f1c4d4ef23b42bf1f556ada5825b02
Scrapss <mbernard@gaming.tech>
2025-06-02 22:06:32 +0200

    Police d'écriture et lumière

---
+++ Assets/Scripts/ScriptMaxence/DeactivateLightSource.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class DeactivateLightSource : MonoBehaviour
+{
+    [SerializeField] private Light2D LightSource;
+    private Color nativeColor;
+    private Color endColor;
+
+    private BoxCollider2D _collider;
+
+    public float a_Duration;
+    private float a_Time = 0;
+
+    public bool Deactivate = false;
+
+    private void Awake()
+    {
+        _collider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Start()
+    {
+        nativeColor = LightSource.color;
+        endColor = new Color(nativeColor.r, nativeColor.g, nativeColor.b, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Deactivate)
+        {
+            LightFadeOut();
+        }
+    }
+
+    void LightFadeOut()
+    {
+        float ratio = a_Time / a_Duration;
+
+        LightSource.color = Color.Lerp(nativeColor, endColor, ratio);
+
+        a_Time += Time.unscaledDeltaTime;
+        if (a_Time > a_Duration)
+        {
+            a_Time = 0;
+            Deactivate = false;
+            Destroy(LightSource.gameObject);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerPower>().CanDeactivateLightSource(this);
+        }
+    }
+}
--- Assets/Scripts/ScriptMaxence/PlayerPower.cs
+++ Assets/Scripts/ScriptMaxence/PlayerPower.cs
@@ -80,12 +80,12 @@ public class PlayerPower : MonoBehaviour
         if (playerController.InShadow)
         {
             //C'est la touche E
-            if (Input.GetButtonDown("Fire2") && !ShadowTP_Stance && playerController.IsGrounded)
+            if (Input.GetKeyDown(KeyCode.Q) && !ShadowTP_Stance && playerController.IsGrounded)
             {
                 ShadowTP_Stance = true;
 
             }
-            else if (Input.GetButtonDown("Fire2") && ShadowTP_Stance)
+            else if (Input.GetKeyDown(KeyCode.Q) && ShadowTP_Stance)
             {
                 ShadowTP_Stance = false;
             }
@@ -202,6 +202,14 @@ public class PlayerPower : MonoBehaviour
         if (Input.GetKey("e"))
         {
             ExecuteEnemy(enemy);
+        }
+    }
+
+    public void CanDeactivateLightSource(DeactivateLightSource LightScript)
+    {
+        if (Input.GetKey("e"))
+        {
+            LightScript.Deactivate = true;
         }
     }
 
